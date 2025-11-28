405d2a17ca1764dbd15605121189154be943ab98
Tamanna <tahminadox@gmail.com>
2025-11-28 15:05:45 +0600

    add sample users array and /users API endpoint

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+const users = [
+    { id: 1, name: "Tamanna", email: "iG7yC@example.com" },
+    { id: 2, name: "John", email: "HnOwE@example.com" },
+    { id: 3, name: "Jane", email: "Xo7oI@example.com" }
+];
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
