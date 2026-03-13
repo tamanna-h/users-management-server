eb107cb3f3f800938d0bf9d9f35e06baffd9d637
Tamanna <tahminadox@gmail.com>
2026-03-13 14:49:40 +0600

    feat: add express.json middleware and parse request body and respond with new user

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -21,6 +22,10 @@ app.get('/users', (req, res) => {
 
 app.post('/users', (req, res) => {
     console.log('users post method');
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    res.send(newUser)
 })
 
 
